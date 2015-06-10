864eed8f68a6c9e705a79adf7dabb7165aa1924e
strucoder <zdwloveschina@163.com>
2015-06-10 23:22:03 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,156 @@
-# img-order
-Customize image to your needs   --  nodejs package
+English
+--------
+Introduction
+---------
+Customize image to your needs
+
+Getting started
+--------
+First, we should download graphicsmagick
+
+Install graphicsmagick
+
+#### Mac OSX
+
+```
+brew install graphicsmagick
+```
+
+#### Linux
+```
+sud apt-get install graphicsmagick
+```
+
+#### Windows
+
+[window install](http://www.graphicsmagick.org/INSTALL-windows.html)  
+
+
+
+#### then either use npm
+npm install img-order
+
+
+How to use ?
+----------
+If you use `experss` or `koa` you just do this
+```javascript
+var app = require('express'); // or app = ('koa')
+var imgOrder = require('img-order');
+
+var config = {
+    dest: 'absolute path to your image dir',    // must
+    url: 'the client request url',              // must
+    legalImg: 'jpg,jpeg,png,gif',               // option
+    tempImgDir: 'cache temp images',            // option(suggest to use)
+    maxAge: 2592000                             // cache-control option
+}
+app.use(imgOrder(config));
+```
+
+Interface specification
+-----------------------
+imageView/`mode`
+        /w/`Width`
+        /h/`Height`
+        /format/`Format`
+| mode        | intruduction    |
+| ------------- |:-------------:|
+| /0/w/`LongEdge`/h/`ShortEdge` |Limit the long sides of the thumbnail for a maximum of `LongEdge`, short edge up to `ShortEdge`, carries on the geometric scaling, not cut. If only specify `w` parameters is limited long (short) and adaptive, specify only `h` parameter indicates limited short side (long edge adaptive). |
+| /1/w/`Width`/h/`Height`| Qualified thumbnail wide at least for ` Width `, high minimum of `Height`, carries on the geometric scaling, center cut. After turning the thumbnail usually happens to be  `Width` x ` Height ` size (one side when zooming by beyond rectangular box was cut off excess part). If you only specify w parameters or only specified h, on behalf of the limited to tetragonal figure is as broad as it is      | 
+| /2/w/`Width`/h/`Height` | Limit the thumbnail Width up to `Width`, high up to `Height`, carries on the geometric scaling, not cut. If only specify w parameter represents finite width (adaptive), specify only h parameter finite long (wide adaptive). It similar to mode 0, the difference between just limit width and height, not qualified long side and short side.      |
+|/3/w/`Width`/h/`Height`|Qualified thumbnail wide at least for `Width`, high minimum of `Height`, carries on the geometric scaling, not cut. If only specify w parameters or only specified h, on behalf of the width is limited to the same value|
+|/4/w/`LongEdge`/h/`ShortEdge`|Limit the long sides of the thumbnail for at least the `LongEdge`, short edge at least for `ShortEdge`, carries on the geometric scaling, not cut. If you only specify w parameters or only specified h, said long should be the same value in a short while|
+|/5/w/`LongEdge`/h/`ShortEdge`|Limit the long sides of the thumbnail for at least the `LongEdge`, short edge at least for `ShortEdge`, carries on the geometric scaling, center cut. If you only specify w parameters or only specified h, said long should be the same value in a short while.|
+|/format/`Format`|The output of the new format|
+
+
+Todo
+-------
+- [x]  thumbnail image
+- [x]  image cut
+- [x]  format image
+- [ ]  rotate image
+- [ ]  progressive display
+- [ ]  watermark
+- [ ]  EXIF(EXchangeable Image File Format)
+
+
+
+
+中文
+---------------
+
+介绍
+----
+根据需求定制自己需要的图片
+
+准备开始
+--------
+首先我们需要下载graphicsmagick
+
+安装graphicsmagick
+
+#### Mac OSX
+
+```
+brew install graphicsmagick
+```
+
+#### Linux
+```
+sud apt-get install graphicsmagick
+```
+
+#### Windows
+
+[install exe](http://www.graphicsmagick.org/INSTALL-windows.html)  
+
+
+
+#### 之后使用npm安装
+npm install img-order  
+
+如何使用 ?
+----------
+If you use `experss` or `koa` you just do this
+```javascript
+var app = require('express'); // or app = ('koa')
+var imgOrder = require('img-order');
+
+var config = {
+    dest: 'absolute path to your image dir',    // 必须
+    url: 'the client request url',              // 必须
+    legalImg: 'jpg,jpeg,png,gif',               // 可选
+    tempImgDir: 'cache temp images',            // 可选(推荐使用)
+    maxAge: 2592000                             // 可选 cache-control
+}
+app.use(imgOrder(config));
+```  
+
+接口规范
+-----------------------
+imageView/`mode`
+        /w/`Width`
+        /h/`Height`
+        /format/`Format`
+| mode        | intruduction    |
+| ------------- |:-------------:|
+| /0/w/`LongEdge`/h/`ShortEdge` |限定缩略图的长边最多为<LongEdge>，短边最多为`ShortEdge`，进行等比缩放，不裁剪。如果只指定 `w` 参数则表示限定长边（短边自适应），只指定 h 参数则表示限定短边（长边自适应） |
+| /1/w/`Width`/h/`Height`| 限定缩略图的宽最少为<Width>，高最少为`Height`，进行等比缩放，居中裁剪。转后的缩略图通常恰好是 `Width`x`Height` 的大小（有一个边缩放的时候会因为超出矩形框而被裁剪掉多余部分）。如果只指定 `w` 参数或只指定 `h` 参数，代表限定为长宽相等的正方图。| 
+| /2/w/`Width`/h/`Height` | 限定缩略图的宽最多为`Width`，高最多为`Height`，进行等比缩放，不裁剪。如果只指定 `w` 参数则表示限定宽（长自适应），只指定 `h` 参数则表示限定长（宽自适应）。它和模式0类似，区别只是限定宽和高，不是限定长边和短边。|
+|/3/w/`Width`/h/`Height`|限定缩略图的宽最少为`Width`，高最少为`Height`，进行等比缩放，不裁剪。如果只指定`w` 参数或只指定`h`. 参数，代表长宽限定为同样的值|
+|/4/w/`LongEdge`/h/`ShortEdge`|限定缩略图的长边最少为`LongEdge`，短边最少为`ShortEdge`，进行等比缩放，不裁剪。如果只指定`w` 参数或只指定 `h` 参数，表示长边短边限定为同样的值|
+|/5/w/`LongEdge`/h/`ShortEdge`|限定缩略图的长边最少为`LongEdge`，短边最少为`ShortEdge`，进行等比缩放，居中裁剪。如果只指定`w` 参数或只指定`h`. 参数，表示长边短边限定为同样的值|  
+
+
+需要完成的任务
+-------
+- [x]  图片的缩略
+- [x]  图片裁剪
+- [x]  不同格式图片输出
+- [ ]  旋转图片
+- [ ]  图片的渐进显示
+- [ ]  水印
+- [ ]  EXIF(EXchangeable Image File Format)
+
---
+++ src/index.js
@@ -0,0 +1,370 @@
+var path = require('path');
+var fs = require('fs');
+var url = require('url');
+var gm = require('gm');
+
+/**
+	* dest: absolute path to images folder
+	* url: get customize image for example: customize-img
+				 and the result is domain.com/customizeImg/image.jpg?imageView/1/w/200/h/200
+	*	legalImg: jpg,jpeg,png,gif
+	* tempImgDir: orderTemp
+	* maxAge: 31536000  
+
+*/
+
+var customizeImg = function(options) {
+	var cache = {};
+	var IMAGE_TYPES = 'jpg,jpeg,png,gif,bmp';
+	var mode, arg;
+	var defaultMaxAge = 60 * 60 * 24 * 30 * 6;// half an year
+	var legalMode = [0, 1, 2, 3, 4, 5];
+	return function(req, res, next) {
+		var parsePath = url.parse(req.url, true);
+		var pathName = parsePath.pathname;
+		if (pathName.indexOf(options.url) === -1) {
+			return next();
+		}
+
+		if (!cache.maxAge) {
+			cache.maxAge = parseInt(options.maxAge, 10) || defaultMaxAge
+		}
+		if (!cache.imageType) {
+			cache.imageType = options.legalImg || IMAGE_TYPES;
+		}
+		if (!cache.legalImg) {
+			cache.legalImg = '(' + cache.imageType.replace(/\,/g, '|') + ')';
+		}
+		if (!cache.tempImgDir) {
+			cache.tempImgDir = options.tempImgDir || null;
+		}
+		var reg = new RegExp("/" + options.url + "/(\\S+\\." + cache.legalImg + ")\?", 'i');
+		var imgArgsArr = pathName.match(reg);
+		if (imgArgsArr.length !== 3) {
+			return endReq('illegal mode', 1);
+		}
+		var imageName = imgArgsArr[1];
+		var mime = 'image/' + imgArgsArr[2];
+		var imagePath = path.join(options.dest, imageName);
+		if (!fs.existsSync(imagePath)) {
+			return endReq('file not exist', 3);
+		}
+		var viewArgs = parsePath.search.substring(parsePath.search.indexOf('/') + 1);
+		var argsArr = viewArgs.toLowerCase().split('/');
+		var argsArrLen = argsArr.length;
+		mode = parseInt(argsArr[0], 10);
+		if (options.tempImgDir) {
+			var cacheImagePath = path.join(cache.tempImgDir, viewArgs.replace(/\//g, '') + imageName);
+		}		
+
+		function endReq(msg, code) {
+			var returnJson = {};
+			code = code || 0;
+			returnJson.code = code;
+			returnJson.msg = msg || "";
+			res.statusCode = 400;
+			return res.end(JSON.stringify(returnJson));
+		}
+
+		var pipeStream = function(stream) {
+			res.setHeader('Content-Type', mime);
+			res.setHeader('Cache-Control', 'public, max-age=' + cache.maxAge);
+			if (cache.tempImgDir) {
+				var writeStream = fs.createWriteStream(cacheImagePath);
+				stream.pipe(writeStream);
+			}
+			stream.pipe(res);
+		}
+
+		// mode0 was format function
+		var mode0 = function(ext) {
+			return gm(imagePath).stream(ext, function(err, stdout) {
+				pipeStream(stdout)
+			});
+		}
+
+		var mode1 = function(w, h) {
+			//等比缩放, 不裁剪
+			gm(imagePath).size(function(err, size) {
+				var originW = size.width;
+				var originH = size.height;
+				if (w && h) {
+					if (w < originW || h < originH) {
+						if (w / h > originW / originH) {
+							w = (originW * h / originH).toFixed(1);
+							this.resize(w, h)
+									.stream(function(err, stdout) {
+										pipeStream(stdout);
+									});
+						} else {
+							h = (w * originH / originW).toFixed(1);
+							this.resize(w, h)
+									.stream(function(err, stdout) {
+										pipeStream(stdout);
+									});
+						}
+					} else {
+						this.stream(function(err, stdout) {
+									pipeStream(stdout);
+								});
+					}
+				} else if (w || h) {
+					var temp = w || h;
+					if (temp > originW || temp > originH) {
+						this.stream(function(err, stdout) {
+							pipeStream(stdout);
+						});
+					}
+					h = h || (temp * originH / originW).toFixed(1);
+					w = w || (originW * temp / originH).toFixed(1);
+					this.resize(w, h)
+							.stream(function(err, stdout) {
+								pipeStream(stdout);
+							});
+				}
+			});
+		} 
+
+		var mode2 = function(w, h) {
+			//等比缩放, 裁剪
+			gm(imagePath).size(function(err, size){
+				var originW = size.width;
+				var originH = size.height;
+				if (w && h) {
+					if (w > originW && h > originH) {
+						this.stream(function(err, stdout) {
+							pipeStream(stdout);
+						});
+					} else {
+						if (w <= originW && h >= originH) {
+							h = originH
+						} else if (w > originW && h < originH){
+							w = originW
+						}
+						// this.out('-thumbnail', w + "x" + h + "^")
+						this.resize(w, h, "^")
+							.gravity("Center")
+							.extent(w, h)
+							.stream(function(err, stdout) {
+								pipeStream(stdout);
+							});
+					}
+				} else {
+					var temp = w || h;
+					this.resize(temp, temp,"^")
+							.gravity("Center")
+							.extent(temp, temp)
+							.stream(function (err, stdout) {
+								pipeStream(stdout)
+							});
+				}
+			});
+		}
+
+		var mode3 = function(w, h) {
+			mode1(w, h);
+		}
+
+		var mode4 = function(w, h) {
+			gm(imagePath).size(function(err, size) {
+				var originW = size.width;
+				var originH = size.height;
+				if (w && h) {
+					if (w > originW && h > originH) {
+						this.stream(function(err, stdout) {
+								pipeStream(stdout);
+							});
+					} else {
+						if (w / h > originW / originH) {
+							h = (w * originH / originW).toFixed(1);
+						} else {
+							w = (h * originW / originH).toFixed(1);
+						}
+						this.resize(w, h)
+								.stream(function(err, stdout) {
+									pipeStream(stdout);
+								});
+					}
+				} else {
+					var temp = w || h;
+					if (temp > originW || temp > originH) {
+						return this.stream(function(err, stdout) {
+								pipeStream(stdout);
+							});
+					}
+					//  1 means w === h 
+					if (1 > originW / originH) {
+						h = (temp * originH / originW).toFixed(1);
+					} else {
+						w = (temp * originW / originH).toFixed(1);
+					}
+
+					this.resize(w, h)
+							.stream(function(err, stdout) {
+								pipeStream(stdout);
+							});
+				}
+			});
+		}
+
+		var mode5 = function(w, h) {
+			gm(imagePath).size(function(err, size) {
+				var longEdje, shortEdje;
+				if (size.width > size.height) {
+					longEdje = size.width;
+					shortEdje = size.height;
+				}	else {
+					shortEdje = size.width;
+					longEdje = size.height;
+				}
+				if (w && h) {
+					if (w / h > longEdje / shortEdje) {
+						h = (w * shortEdje / longEdje).toFixed(1);
+					} else {
+						w  = (longEdje * h / shortEdje).toFixed(1);
+					}
+					this.resize(w, h)
+							.stream(function(err, stdout) {
+								pipeStream(stdout);
+							});
+				} else {
+					var temp = w || h;
+					if (temp > longEdje) {
+						return this.stream(function(err, stdout) {
+								pipeStream(stdout);
+							});
+					}
+					//  1 means w === h 
+					if (1 > longEdje / shortEdje) {
+						h = (temp * shortEdje / longEdje).toFixed(1);
+					} else {
+						w = (temp * longEdje / shortEdje).toFixed(1);
+					}
+
+					this.resize(w, h)
+							.stream(function(err, stdout) {
+								pipeStream(stdout);
+							});
+				}				
+			});
+		}
+
+		var mode6 = function(w, h) {
+			gm(imagePath).size(function(err, size) {
+				var longEdje, shortEdje;
+				if (size.width > size.height) {
+					longEdje = size.width;
+					shortEdje = size.height;
+				}	else {
+					shortEdje = size.width;
+					longEdje = size.height;
+				}
+				if (w && h) {
+					if (w > longEdje && h > shortEdje) {
+						this.resize(w, h)
+								.stream(function(err, stdout) {
+									pipeStream(stdout);
+								});
+					} else {
+						this.resize(w, h, "^")
+								.gravity('Center')
+								.extent(w, h)
+								.stream(function(err, stdout) {
+									pipeStream(stdout);
+								});
+					}
+				} else {
+					var temp = w || h;
+					if (temp > longEdje) {
+						return this.stream(function(err, stdout) {
+								pipeStream(stdout);
+							});
+					}
+					this.resize(temp, temp, "^")
+							.gravity('Center')
+							.extent(temp,temp)
+							.stream(function(err, stdout) {
+								pipeStream(stdout);
+							});
+				}			
+			});
+		}
+
+
+		var parpareDeal = function() {
+			if (cache.tempImgDir && fs.existsSync(cacheImagePath)) {
+				res.setHeader('Content-Type', mime);
+				res.setHeader('Cache-Control', 'public, max-age=' + cache.maxAge);
+				var readStream = fs.createReadStream(cacheImagePath);
+				return readStream.pipe(res);
+			}
+			if (argsArr.indexOf("format") !== -1) {
+				var imgExt = argsArr[1];
+				if (cache.imageType.indexOf(imgExt) === -1 && argsArr.length === 2) {
+					return endReq('illegal image types', 8);
+				}
+				return mode0(imgExt);
+			}
+			if (legalMode.indexOf(mode) === -1) {
+				return endReq('illegal mode', 1);
+			}
+
+			if (argsArrLen < 3) {
+				return endReq('illegal args', 2);
+			}
+
+			var indexOfW = argsArr.indexOf('w');
+			var indexOfH = argsArr.indexOf('h');
+			var w, h;
+			if (argsArrLen === 3) {
+				if (indexOfW !== -1) {
+					w = parseInt(argsArr[indexOfW + 1], 10);
+					h = null;
+					if (isNaN(w)) {
+						return endReq('illegal arg w', 1);
+					}
+				} else {
+					h = parseInt(argsArr[indexOfH + 1], 10);
+					if (isNaN(h)) {
+						return endReq('illegal arg h', 1);
+					}
+					w = null;
+				}
+			} else if (argsArrLen === 5) {
+					w = parseInt(argsArr[indexOfW + 1], 10);
+					h = parseInt(argsArr[indexOfH + 1], 10);
+					if (isNaN(w) || isNaN(h)) {
+						return endReq('illegal args', 1);		
+					}
+			} else {
+				return endReq('illegal mode', 1);
+			}
+			
+			switch (mode) {
+				case 0:
+					mode1(w, h);
+					break;
+				case 1:
+					mode2(w, h);
+					break;
+				case 2:
+					mode3(w, h);
+					break;
+				case 3:
+					mode4(w, h);
+					break;
+				case 4:
+					mode5(w, h);
+					break;
+				case 5:
+					mode6(w, h);
+					break;
+				default:
+					endReq('end', 9);
+			}
+		}
+		parpareDeal();
+	}
+}
+
+module.exports = customizeImg;
